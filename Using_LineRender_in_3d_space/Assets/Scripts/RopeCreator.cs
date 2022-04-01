8f824d75ce4f082b24717cf191f1aeb7e1c2306b
LeeSeongho <hoanart@naver.com>
2022-04-01 17:30:40 +0900

    add Creator script

---
+++ Using_LineRender_in_3d_space/Assets/Scripts/RopeCreator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeCreator : MonoBehaviour
+{
+    public Transform startTr;
+    public Transform endTr;
+
+    public LineRenderer lineRenderer;
+
+    public int segmentCount = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
